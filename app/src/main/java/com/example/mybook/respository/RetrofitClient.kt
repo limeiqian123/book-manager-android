a780d86bad2f0af146859850ba13ab531288693a
caichangting <1518905131@qq.com>
2024-02-19 17:31:39 +0800

    update api path

--- app/src/main/java/com/example/mybook/respository/RetrofitClient.kt
+++ app/src/main/java/com/example/mybook/respository/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://20.220.138.254/book/"
+    private const val BASE_URL = "http://20.220.54.22/book/"
 
     fun getInstance(): ApiService {
         return Retrofit.Builder()
